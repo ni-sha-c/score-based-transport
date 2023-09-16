53524719e71e3cf282753ee96569bfeeaa03e59a
Nisha Chandramoorthy <nishac@mit.edu>
2023-09-16 18:59:00 -0400

    Add two pdes

--- NeuralPDE/test_neurodiffeq.py
+++ NeuralPDE/test_neurodiffeq.py
@@ -1,7 +1,7 @@
 from neurodiffeq import diff
 from neurodiffeq.solvers import Solver1D, Solver2D
 from neurodiffeq.conditions import IVP, DirichletBVP2D
-from neurodiffeq.networks import FCNN, SinActv
+from neurodiffeq.networks import FCNN
 import torch
 import numpy as np
 from matplotlib.pyplot import *
@@ -10,25 +10,25 @@ def target_score(x):
 def d_target_score(x):
     return array([2*x, 0, 0, 3*x**2]).reshape(2, 2)
 def pde_x(u, x, y):
-    return [diff(u[0], x, order=2) + diff(u[0], y, order=1)]
+    return [diff(u, x, order=2) + diff(u, y, order=1)]
 def pde_y(u, x, y):
-    return [diff(u[0], x, order=1) + diff(u[0], y, order=2)]
+    return [diff(u, x, order=1) + diff(u, y, order=2)]
 conditions = [
     DirichletBVP2D(
-        x_min=0, x_min_val=lambda y: [torch.sin(np.pi*y), 0],
-        x_max=1, x_max_val=lambda y: [0,0],                   
-        y_min=0, y_min_val=lambda x: [0,0],                   
-        y_max=1, y_max_val=lambda x: [0,0]                   
+        x_min=0, x_min_val=lambda y: torch.sin(np.pi*y),
+        x_max=1, x_max_val=lambda y: 0,                   
+        y_min=0, y_min_val=lambda x: 0,                   
+        y_max=1, y_max_val=lambda x: 0                   
     )
 ]
-nets = [FCNN(n_input_units=2, n_output_units=2, hidden_units=(512,))]
+net_x = [FCNN(n_input_units=2, n_output_units=1, hidden_units=(512,))]
+net_y = [FCNN(n_input_units=2, n_output_units=1, hidden_units=(512,))]
 
-solver = Solver2D(pde_system, conditions, xy_min=(0, 0), xy_max=(1, 1), nets=nets)
+solver = Solver2D(pde_x, conditions, xy_min=(0, 0), xy_max=(1, 1), nets=net_x)
 solver.fit(max_epochs=2000)
 solution = solver.get_solution()
 x, y = np.meshgrid(np.linspace(0, 1, 100), np.linspace(0, 1, 100))
 u = solution(x, y, to_numpy=True)
-"""
 fig, ax = subplots()
 sol = ax.contourf(x, y, u)
 cbar = fig.colorbar(sol)
@@ -41,4 +41,4 @@ ax.xaxis.set_tick_params(labelsize=20)
 ax.yaxis.set_tick_params(labelsize=20)
 fig.tight_layout()
 show()
-"""
+
